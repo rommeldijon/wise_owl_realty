9d430286021f546d7d16a6484ed669dcca9d1ed5
Rommel Dijon <bartmilhausky@gmail.com>
2026-05-21 13:54:08 -0700

    Implemented Tab Navigation

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,3 @@
-import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -11,10 +10,6 @@ export default function Index() {
       }}
     >
        <Text className="font-bold text-lg my-10 font-rubik text-3xl">Welcome to Wise Owl</Text>
-       <Link href="/sign-in">Sign In</Link>
-       <Link href="/explore">Explore</Link>
-       <Link href="/profile">Profile</Link>
-       <Link href="/properties/1">Property</Link>
     </View>
   );
 }
