7eacb2d1f585391a48dab396d6711cf60909193a
Rommel Dijon <bartmilhausky@gmail.com>
2026-05-24 16:16:12 -0700

    Added search bar in home screen/ index

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,15 +1,26 @@
-import { Text, View } from "react-native";
+import Search from '@/components/Search';
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { Image, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-       <Text className="font-bold text-lg my-10 font-rubik text-3xl">Welcome to Wise Owl</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+       
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row items-center">
+            <Image source={images.avatar} className="size-12 rounded-full"/>
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
+              <Text className="text-base font-rubik-medium text-black-300"> Rommel </Text>
+            </View>
+          </View>
+          <Image source={icons.bell} className="size-6" />
+        </View>
+         <Search/>
+      </View>
+    </SafeAreaView>
   );
 }
