6bf5c28595a76a9701f8cd46bfbd368d84dfbd46
Rommel Dijon <bartmilhausky@gmail.com>
2026-05-25 15:05:38 -0700

    Implemented Flatlists in HomeScreen UI

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -23,7 +23,9 @@ const Filters = () => {
   return (
     <ScrollView horizontal showsHorizontalScrollIndicator={false} className='mt-3 mb-2'>
         {categories.map((item, index) => (
-            <TouchableOpacity onPress={() => handleCategoryPress(item.category)} 
+            <TouchableOpacity 
+             key={item.category || index}
+             onPress={() => handleCategoryPress(item.category)} 
             className={`flex flex-col items-start mr-4 px-4 py-2 rounded-full
             ${selectedCategory === item.category? 'bg-primary-300' : 
             'bg-primary-100 border border-primary-200'}`}>
