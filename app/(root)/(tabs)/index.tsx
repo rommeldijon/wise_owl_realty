3e9973b56b82a25913755223934e19178e000f8f
Rommel Dijon <bartmilhausky@gmail.com>
2026-05-25 10:08:43 -0700

    Initial rendering of Cards component

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -29,9 +29,24 @@ export default function Index() {
               <Text className='text-base font-rubik-bold text-primary-300'>See All</Text>
             </TouchableOpacity>
           </View>
+
+          <View className="flex flex-row gap-5 mt-5">
+            <FeaturedCard/>
+            <FeaturedCard/>
+          </View>
         </View>
-        <FeaturedCard/>
-        <Card/>
+
+        <View className='flex flex-row items-center justify-between'>
+            <Text className='text-xl font-rubik-bold text-black-300'>Recommendations</Text>
+            <TouchableOpacity>
+              <Text className='text-base font-rubik-bold text-primary-300'>See All</Text>
+            </TouchableOpacity>
+        </View>
+
+        <View className="flex flex-row gap-5 mt-5">
+            <Card/>
+            <Card/>
+        </View>
 
       </View>
     </SafeAreaView>
