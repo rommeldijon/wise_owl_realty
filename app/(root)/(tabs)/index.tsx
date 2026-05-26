943625843c4adace79c301d037893d9558436756
Rommel Dijon <bartmilhausky@gmail.com>
2026-05-26 14:05:50 -0700

    Rendered display data on Home UI

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,6 @@
+import { router, useLocalSearchParams } from "expo-router";
 import {
+  ActivityIndicator,
   FlatList,
   Image,
   Text,
@@ -9,14 +11,49 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 import { Card, FeaturedCard } from '@/components/Cards';
 import Filters from "@/components/Filters";
+import NoResults from "@/components/NoResults";
 import Search from '@/components/Search';
 import icons from "@/constants/icons";
+import { getLatestProperties, getProperties } from "@/lib/appwrite";
 import { useGlobalContext } from "@/lib/global-provider";
-import React from "react";
+import { useAppwrite } from "@/lib/useAppwrite";
+import React, { useEffect } from "react";
 
 export default function Index() {
     const { user } = useGlobalContext();
     const [avatarLoadFailed, setAvatarLoadFailed] = React.useState(false);
+    const params = useLocalSearchParams<{ query?: string; filter?: string }>();
+
+    const { 
+      data: latestProperties, 
+      loading: latestPropertiesLoading } =
+      useAppwrite({
+      fn: getLatestProperties,
+    });
+
+    const {
+      data: properties,
+      refetch,
+      loading,
+    } = useAppwrite({
+      fn: getProperties,
+      params: {
+        filter: params.filter!,
+        query: params.query!,
+        limit: 6,
+      },
+      skip: true,
+    });
+
+    useEffect(() => {
+      refetch({
+        filter: params.filter!,
+        query: params.query!,
+        limit: 6,
+      });
+    }, [params.filter, params.query]);
+    
+    const handleCardPress = (id: string) => router.push(`/properties/${id}`);
 
     const initialsSource = user?.name?.trim() || user?.email?.trim() || "";
     const userInitials = initialsSource
@@ -33,13 +70,22 @@ export default function Index() {
   return (
     <SafeAreaView className="bg-white h-full">
       <FlatList
-          data={[1,2, 3, 4]}
-          keyExtractor={(item) => item.toString()}
+          data={properties ?? []}
+          keyExtractor={(item, index) => item?.$id ?? item?.id ?? index.toString()}
           numColumns={2}
           contentContainerClassName="pb-32"
           columnWrapperClassName="flex gap-5 px-5"
-          renderItem={({ item }) => <Card/>}
+          renderItem={({ item }) =>  (
+             <Card item={item} onPress={() => handleCardPress(item.$id)} />
+          )}
           showsVerticalScrollIndicator={false}
+          ListEmptyComponent={
+            loading ? (
+              <ActivityIndicator size='large' className="text-primary-300 mt-5"/>
+            ) : (
+            <NoResults/>
+            )
+          }
           ListHeaderComponent={
         <View className="px-5">
         
@@ -85,32 +131,42 @@ export default function Index() {
               </TouchableOpacity>
             </View>
 
-            <FlatList
-              data={[1, 2, 3]}
-              renderItem={({item}) => <FeaturedCard/>}
-              keyExtractor={(item) => item.toString()}
+            {latestPropertiesLoading ? (
+              <ActivityIndicator size="large" className="text-primary-300" />
+            ) : !latestProperties || latestProperties.length === 0 ? (
+              <NoResults />
+            ) : (
+             <FlatList
+              data={latestProperties ?? []}
+              renderItem={({ item }) => (
+                    <FeaturedCard
+                      item={item}
+                      onPress={() => handleCardPress(item.$id)}
+                    />
+                  )}
+              keyExtractor={(item, index) => item?.$id ?? item?.id ?? index.toString()}
               horizontal
               bounces={false}
               showsHorizontalScrollIndicator={false}
               contentContainerClassName="flex gap-5 mt-5"
             />
-
+           )}
           </View>
-
           <View className='flex flex-row items-center justify-between'>
-              <Text className='text-xl font-rubik-bold text-black-300'>Recommendations</Text>
+              <Text className='text-xl font-rubik-bold text-black-300'>
+                Recommendations
+              </Text>
               <TouchableOpacity>
-                <Text className='text-base font-rubik-bold text-primary-300'>See All</Text>
+                <Text className='text-base font-rubik-bold text-primary-300'>
+                  See All
+                </Text>
               </TouchableOpacity>
           </View>
 
-          <Filters/>
-
-          
-
+          <Filters/>          
         </View>
-        }
+      }
       />
     </SafeAreaView>
   );
-}
+};
--- components/Cards.tsx
+++ components/Cards.tsx
@@ -8,27 +8,29 @@ import {
 
 import icons from '@/constants/icons';
 import images from '@/constants/images';
+import { Models } from 'react-native-appwrite';
 
 interface Props {
+    item: Models.Document;
     onPress?: () => void;
 }
 
-export const FeaturedCard= ({ onPress }: Props) => {
+export const FeaturedCard= ({ item:{image, rating, name, address, price }, onPress }: Props) => {
   return (
     <TouchableOpacity onPress={onPress} className='flex flex-col items-start w-60 h-80 relative'>
-        <Image source={images.japan} className='size-full rounded-2xl'/>
+        <Image source={{ uri: image }} className='size-full rounded-2xl'/>
         <Image source={images.cardGradient} className='size-full rounded-2xl absolute bottom-0'/>
         
         <View className='flex flex-row items-center bg-white/90 px-3 py-1.5 rounded-full absolute top-5 right-5'>
             <Image source={icons.star} className='size-3.5'/>
-            <Text className='text-xs font-rubik-bold text-primary-300 ml-1'>4.4</Text>
+            <Text className='text-xs font-rubik-bold text-primary-300 ml-1'>{rating}</Text>
         </View>
 
         <View className='flex flex-col items-start absolute bottom-5 inset-x-5'>
-            <Text className='text-xl font-rubik-extrabold text-white' numberOfLines={1}>Modern Apartment</Text>
-            <Text className='text-base font-rubik text-white'>22 W. Fujiwara St, Tokyo,JPN</Text>
+            <Text className='text-xl font-rubik-extrabold text-white' numberOfLines={1}>{name}</Text>
+            <Text className='text-base font-rubik text-white'>{address}</Text>
             <View className='flex flex-row items-center justify-between w-full'>
-                <Text className='text-xl font-rubik-extrabold text-white'>$2,500</Text>
+                <Text className='text-xl font-rubik-extrabold text-white'>{price}</Text>
                 <Image source={icons.heart} className='size-5'/>
             </View>
         </View>
@@ -36,7 +38,7 @@ export const FeaturedCard= ({ onPress }: Props) => {
   )
 }
 
-export const Card =({ onPress }: Props) => {
+export const Card = ({ item:{image, rating, name, address, price }, onPress }: Props) => {
     return (
         <TouchableOpacity onPress={onPress} className='flex-1 w-full mt-4 px-3 py-4 
            rounded-lg bg-white shadow-lg shadow-black-100/70 relative'
@@ -48,19 +50,19 @@ export const Card =({ onPress }: Props) => {
                 </Text>
            </View>
 
-           <Image source={images.newYork} className='w-full h-40 rounded-lg'/>
+           <Image source={{ uri: image}} className='w-full h-40 rounded-lg'/>
 
            <View className='flex flex-col mt-2'>
                 <Text className='text-base font-rubik-bold text-black-300'>
-                    Cozy Studio
+                  {name}
                 </Text>
                 <Text className='text-xs font-rubik text-black-200'>
-                    101 W. 34th St, NYC, NY
+                    {address}
                 </Text>
 
                 <View className='flex flex-row items-center justify-between mt-2'>
                     <Text className='text-base font-rubik-bold text-primary-300'>
-                        $3,500
+                        {price}
                     </Text>
                     <Image 
                     source={icons.heart} 
